21356f2a7c21431b9092a99eab0178671ffb209f
idee1140 <idee1140@gmail.com>
2021-10-09 09:42:23 +0100

    initial commit

---
+++ app/src/main/java/com/example/haggle_x/ui/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.example.haggle_x.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.haggle_x.MainActivity
+import com.example.haggle_x.R
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private lateinit var mainActivity: MainActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainActivity = activity as MainActivity
+    }
+
+
+}
